fa300013e870d8756678d3a6e40d9719c82befd8
zelca <zelcadev@gmail.com>
2016-05-31 08:55:43 +0200

    Implement Text Learning mini-project

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,21 +41,27 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        #temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            text = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            filtered = text
+            for word in ["sara", "shackleton", "chris", "germani", "sshacklensf", "cgermannsf"]:
+                if word in filtered:
+                    filtered = filtered.replace(word,"")
 
-            ### append the text to word_data
+                    ### append the text to word_data
+                    word_data.append(filtered)
 
-            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+                    ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+                    from_data.append(0 if name == "sara" else 1)
 
             email.close()
 
@@ -63,13 +69,16 @@ print "emails processed"
 from_sara.close()
 from_chris.close()
 
+print "word_data[152]: ", word_data[152]
+
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
-
-
-
-
 ### in Part 4, do TfIdf vectorization here
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words="english").fit(word_data)
+
+print "vacabulary size: ", len(vectorizer.get_feature_names())
+print "word 34597 in vocabulary: ", vectorizer.get_feature_names()[34597]
 
