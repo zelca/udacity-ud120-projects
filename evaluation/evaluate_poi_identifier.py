c2f596a8ff28415ec6894d2260058e5e18e1c17a
zelca <zelcadev@gmail.com>
2016-06-09 00:01:53 +0200

    Implement Evaluation Metrics mini-project

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -24,8 +24,33 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
-
-
 ### your code goes here 
+
+from sklearn import tree
+from sklearn.cross_validation import train_test_split
+
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features, labels, test_size=0.3, random_state=42)
+
+print "total number in test set:", len(labels_test)
+print "poi number in test set:", sum(labels_test)
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+print "accuracy:", clf.score(features_test, labels_test)
+
+labels_predicted = clf.predict(features_test)
+
+true_positives = 0
+for p, t in zip(labels_predicted, labels_test):
+    if p == 1 and t == 1:
+        true_positives += 1
+print "true positives:", true_positives
+
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+
+print "recall score:", recall_score(labels_test, labels_predicted)
+print "precision score:", precision_score(labels_test, labels_predicted)
 
 
